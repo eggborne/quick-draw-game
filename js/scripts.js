70dbf43eed2078e329cf6a8504da8923a00f7e6e
Mike Donovan <mike@mikedonovan.dev>
2023-01-18 20:52:21 -0800

    Add CSS variables for screen/sprite size and JS functions to set

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
 window.addEventListener('load', () => {
+  document.documentElement.style.setProperty('--actual-height', window.innerHeight + 'px');
+  let pixelSize = (window.innerWidth / 170);
+  gameScreenHeight = Math.round(pixelSize * 120);
+  spriteHeight = Math.round(pixelSize * 28);
+  console.warn('pixelSize:', pixelSize);
+  document.documentElement.style.setProperty('--nes-pixel-size', pixelSize + 'px');
+  document.documentElement.style.setProperty('--game-screen-height', gameScreenHeight + 'px');
+  document.documentElement.style.setProperty('--sprite-height', spriteHeight + 'px');
   console.log('window loaded');
   document.querySelector('main').addEventListener('pointerdown', handleClick);
 });
